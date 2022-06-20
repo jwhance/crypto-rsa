e9c91a267c0360a522ed71f61e1d5202eb63aced
Joe Hance <jwh20@hotmail.com>
2022-06-20 09:19:27 -0400

    Update with Python code

--- src/python/encrypt.py
+++ src/python/encrypt.py
@@ -1,11 +1,20 @@
+import base64
 import rsa
 
+# Convert keys from PCKS#8 to PKCS#1 and back
+# https://stackoverflow.com/questions/18039401/how-can-i-transform-between-the-two-styles-of-public-key-format-one-begin-rsa
+
+# RSA Cryto in Python
+# https://www.section.io/engineering-education/rsa-encryption-and-decryption-in-python/
+
+
 def loadKeys():
-    with open('keys/publicKey.pem', 'rb') as p:
+    with open('keys/rsapublicKey.pem', 'rb') as p:
         publicKey = rsa.PublicKey.load_pkcs1(p.read())
     with open('keys/privateKey.pem', 'rb') as p:
         privateKey = rsa.PrivateKey.load_pkcs1(p.read())
     return privateKey, publicKey
+
 
 def generateKeys():
     (publicKey, privateKey) = rsa.newkeys(1024)
@@ -15,5 +24,24 @@ def generateKeys():
         p.write(privateKey.save_pkcs1('PEM'))
 
 
+def rsa_decrypt(ciphertext, key):
+    # Base64 decode ciphertext
+    b64_decoded = base64.b64decode(ciphertext)
+
+    try:
+        # Decrypt and then return an ASCII string from the bytes returned by
+        return rsa.decrypt(b64_decoded, key).decode('ascii')
+
+    except Exception as e:
+        print(e)
+        return False
+
+
+CIPHERTEXT = 'ji4gBygY5OcJ69XPvhm7asNM61s5CCVU6RVW3uyUQqcO37kXvKX/5J25L6OPXQj5Vb767csza1Qcb92CYMWrHk4YSQGIeUgbni86JpnB1WxQv4nhkl9LsxO3LzKdnP7OtTMRUU+Q3mKszgGRuCZwxklmxaAveh/qcYTT8R8v6nM='
+
 # generateKeys()
-loadKeys()
+privateKey, publicKey = loadKeys()
+print('Loaded keys')
+
+plaintext = rsa_decrypt(CIPHERTEXT, privateKey)
+print('Plaintext:', plaintext)
